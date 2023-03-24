18380a4b890de5d1211d0a5508ae4c391dcedead
Bams <bbg2021@gmail.com>
2023-03-24 09:49:24 +0800

    add header

--- resources/views/tampil-mahasiswa.blade.php
+++ resources/views/tampil-mahasiswa.blade.php
@@ -9,15 +9,25 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-
-<div class="px-4 sm:px-6 lg:px-8 container max-5xl mx-auto">
+<header>
+    <section class=" bg-blue-400 ">
+        <div class="max-w-5xl mx-auto py-3">
+            <h1 class="text-3xl">Rental Mobil</h1>
+        </div>
+    </section>
+</header>
+<div class="max-w-5xl mx-auto pt-4">
     <div class="sm:flex sm:items-center">
         <div class="sm:flex-auto">
             <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
-            <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>
+            <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title,
+                email and role.</p>
         </div>
         <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-            <button type="button" class="block rounded-md bg-indigo-600 py-2 px-3 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add user</button>
+            <button type="button"
+                    class="block rounded-md bg-indigo-600 py-2 px-3 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Add user
+            </button>
         </div>
     </div>
     <div class="mt-8 flow-root">
@@ -26,7 +36,9 @@
                 <table class="min-w-full divide-y divide-gray-300">
                     <thead>
                     <tr>
-                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Name</th>
+                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                            Name
+                        </th>
                         <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold text-gray-900">NIM</th>
                         <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold text-gray-900">IPK</th>
 
@@ -37,15 +49,15 @@
                     </thead>
                     <tbody class="divide-y divide-gray-200">
                     @forelse( $mahasiswas as $mhs )
-                    <tr>
-                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">{{ $mhs->nama }}</td>
-                        <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">{{ $mhs->nim }}</td>
-                        <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">{{ $mhs->ipk }}</td>
+                        <tr>
+                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">{{ $mhs->nama }}</td>
+                            <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">{{ $mhs->nim }}</td>
+                            <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">{{ $mhs->ipk }}</td>
 
-                        <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                            <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
-                        </td>
-                    </tr>
+                            <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
+                            </td>
+                        </tr>
                     @empty
                         <tr>
                             <td colspan="5">No data</td>
